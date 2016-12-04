6fd2723c8417e404ef38df069828cbb4ddad81a9
Orlando <cuximyamaorlandodejesus@gmail.com>
2016-12-04 16:13:25 -0600

    ejercicio de esta unidad

---
+++ funciones-variables/ejercicio.php
@@ -0,0 +1,13 @@
+<?php
+
+function funcion_var(){
+  echo("soy una funcion");
+}
+
+function funcion_dos(){
+  echo("soy una funcion");
+}
+
+$fvariable = 'funcion_var';
+$fvariable = 'funcion2';
+//se llama la funcion por que le asigno un nombre igual que al de la funcion
